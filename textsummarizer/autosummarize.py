18d2b8bd8c9744c4922e47c16146010bb3efee5f
Amy Jiang <amyjiang@Amys-MBP.fios-router.home>
2020-10-29 22:29:20 -0400

    Made article downloader a reusable function

--- textsummarizer/autosummarize.py
+++ textsummarizer/autosummarize.py
@@ -3,14 +3,19 @@ from bs4 import BeautifulSoup
 
 articleURL = "https://www.nytimes.com/2020/04/10/world/canada/coronavirus-canada-detroit-nurses-hospital.html"
 
-page = urlopen(articleURL).read().decode('utf8','ignore')
-soup = BeautifulSoup(page,"lxml")
+def getTextArticle(url):
+    page = urlopen(articleURL).read().decode('utf8','ignore')
+    soup = BeautifulSoup(page,"lxml")
+
+    # Collecting all articles
+    text = ' '.join(map(lambda p: p.text, soup.find_all('article')))
+
+    return text
+    
+    # Removing funny characters before return
+    # return text.encode('ascil', errors = 'replace').replace("?"," ")
 
 # Finding First Article
-soup.find('article').text
+# soup.find('article').text
 
-# Collecting all articles
-text = ' '.join(map(lambda p: p.text, soup.find_all('article')))
-
-# Removing funny characters
-text.encode('ascil', errors = 'replace').replace("?"," ")
+print(getTextArticle(articleURL))
