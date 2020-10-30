976185d7c2fc011c7255fc6c77b10a7051f8a1e9
Amy Jiang <amyjiang@Amys-MBP.fios-router.home>
2020-10-29 22:19:49 -0400

    Downloaded articleURL to autosummarize.py

---
+++ textsummarizer/autosummarize.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+articleURL = "https://www.nytimes.com/2020/04/10/world/canada/coronavirus-canada-detroit-nurses-hospital.html"
+
+page = urlopen(articleURL).read().decode('utf8','ignore')
+soup = BeautifulSoup(page,"lxml")
+
+# Finding First Article
+soup.find('article').text
+
+# Collecting all articles
+text = ' '.join(map(lambda p: p.text, soup.find_all('article')))
+print(text)
