30788fb1e953dab91d4d3b35dd7e9f64ed0f85f8
Amy Jiang <amyjiang@Amys-MBP.fios-router.home>
2020-11-05 20:55:45 -0500

    Add code for auto summarizer method

--- textsummarizer/autosummarize.py
+++ textsummarizer/autosummarize.py
@@ -1,7 +1,24 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import nlbasics
+from pprint import pprint
 
-articleURL = "https://www.nytimes.com/2020/04/10/world/canada/coronavirus-canada-detroit-nurses-hospital.html"
+# Tokenizing 
+from nltk.tokenize import word_tokenize, sent_tokenize
+# Stopwords 
+from nltk.corpus import stopwords
+from string import punctuation
+# Bigrams 
+from nltk.collocations import *
+# Stemming 
+from nltk.stem.lancaster import LancasterStemmer
+# Disambiguation 
+from nltk.corpus import wordnet as wn
+from nltk.wsd import lesk
+# Frequency Distribution
+from nltk.probability import FreqDist
+from heapq import nlargest
+from collections import defaultdict
 
 def getTextArticle(url):
     page = urlopen(articleURL).read().decode('utf8','ignore')
@@ -11,11 +28,30 @@ def getTextArticle(url):
     text = ' '.join(map(lambda p: p.text, soup.find_all('article')))
 
     return text
-    
-    # Removing funny characters before return
-    # return text.encode('ascil', errors = 'replace').replace("?"," ")
 
-# Finding First Article
-# soup.find('article').text
+def summarize(text, n):
+    sents = sent_tokenize(text)
 
-print(getTextArticle(articleURL))
+    assert n <= len(sents)
+    word_sent = word_tokenize(text.lower())
+    _stopwords = set(stopwords.words('english') + list(punctuation))
+
+    word_sent = [word for word in word_sent if word not in _stopwords]
+    freq = FreqDist(word_sent)
+    # nlargest(10, freq, key = freq.get)
+
+    ranking = defaultdict(int)
+
+    for i,sent in enumerate(sents):
+        for w in word_tokenize(sent.lower()):
+            if w in freq:
+                ranking[i] += freq[w]
+
+    sents_idx = nlargest(n, ranking, key=ranking.get)
+
+    return [sents[j] for j in sorted(sents_idx)]
+
+if __name__ == '__main__':
+    articleURL = "https://www.nytimes.com/2020/04/10/world/canada/coronavirus-canada-detroit-nurses-hospital.html"
+    text = getTextArticle(articleURL)
+    pprint(summarize(text, 3))
